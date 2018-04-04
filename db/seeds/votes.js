5225b1e89550a4cc1ec94d510e0b8fbe8ab26b53
Kirapan <pxqpxqpxq@hotmail.com>
2018-04-04 22:20:56 +0000

    built / route

--- db/seeds/votes.js
+++ db/seeds/votes.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('votes').del()
     .then(function () {
       return Promise.all([
-        knex('votes').insert({id:1, option_id:1, name: 'Alice', points: 1}),
-        knex('votes').insert({id:2, option_id:2, name: 'Bob', points: 2}),
-        knex('votes').insert({id:3, option_id:1, name: 'Charlie', points: 4})
+        knex('votes').insert({id: 1, option_id:1,name: "Alice",points: 1}),
+        knex('votes').insert({id: 1, option_id:1,name: "Alice",points: 3}),
+        knex('votes').insert({id: 1, option_id:1,name: "Alice",points: 4})
       ]);
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,26 @@ module.exports = (knex) => {
 
   router.get("/", (req, res) => {
     knex
-      .select("*")
-      .from("users")
+      .select("name")
+      .from("polls")
       .then((results) => {
         res.json(results);
     });
   });
+  
+  router.get("votes/:id", (req, res) => {
+    console.log(req.params.id);
+    knex
+      .select('polls.name','options.name')
+      .from("polls")
+      .where('id',req.params.id)
+      .innerjoin('options', 'polls.id', '=', 'options.poll_id')
+      .then((results) => {
+        res.json(results);
+        console.log(results);
+      });
+  });
+
 
   return router;
 }
